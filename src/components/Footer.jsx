8597e0f37496b145848a3569ca63d18fe8d2c31c
lahcen001e <lahcen0000lh@gmail.com>
2022-06-05 11:13:55 +0100

    update 4

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
         <div className='text-center p-10 mt-10 border-t dark:border-gray-700'>
 
        
-        <h1>2022 Goggl , Inc.</h1>
+        <h1>2022 Goggl , Inc. <a href="http://lahcen.click" target="_blank">lahcen.click</a></h1>
         </div>
     )
 }
