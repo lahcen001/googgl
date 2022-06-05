5f8a5bbe3a333e68e159d5d2518f4b7653508470
lahcen001e <lahcen0000lh@gmail.com>
2022-06-05 11:07:06 +0100

    update 3

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -24,13 +24,13 @@ export const Search = () => {
     />
    
 
-   {!text && (
-{/* <button type="button" className ="absolute top-1.5 right-4 text-2xl  text-gray-500" onClick={()=>setText("")}>
+   {/* {!text && (
+<button type="button" className ="absolute top-1.5 right-4 text-2xl  text-gray-500" onClick={()=>setText("")}>
 x
-</button> */}
+</button>
    )
 
-   }
+   } */}
   <Links />
   </div>;
 };
